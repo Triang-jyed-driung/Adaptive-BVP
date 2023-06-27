40edc96a2097f676ff601dbf800bafd47e741c72
Triang-jyed-driung <62597758+Triang-jyed-driung@users.noreply.github.com>
2023-06-27 19:50:24 +0800

    Add files via upload

--- BVPSolver.py
+++ BVPSolver.py
@@ -3,7 +3,7 @@ from math import *
 from numpy.linalg import lstsq, pinv
 import numpy.polynomial.chebyshev as cheb
 import random
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 inv = pinv
 
@@ -405,104 +405,3 @@ def NewtonNonlinearSolver(
     print(f"doesn't converge after {iters} steps!!")
     return u1_root
 
-
-if __name__ == "__main__":
-
-    from scipy.special import jv
-    root = LinearBVPSolver(
-        lambda x: 1/x,
-        lambda x: 1-(100/x)**2,
-        lambda x: 0,
-        0, 600,
-        1,0,0,
-        1,0,1,
-        TOL=5e-10,
-        eval_points=100
-    )
-    xs = np.linspace(0,600,600)
-    plt.plot(xs, [(root(xi) - jv(100,xi)/jv(100,600)) for xi in xs])
-    plt.title("Bessel 100 on [0,600]")
-    plt.savefig("bessel.jpg")
-    plt.show()
-    
-
-    root = LinearBVPSolver(
-        lambda x: 0,
-        lambda x: 1-x**2,
-        lambda x: 0,
-        -6,6,
-        1,0,exp(-18),
-        1,0,exp(-18),
-        TOL=1e-3,
-    )
-    xs = np.linspace(-6,6,100)
-    plt.plot(xs, [(root(xi) - exp(-xi**2/2)) for xi in xs])
-    plt.title("u''+(1-x^2)u=0, exp(-x^2/2)")
-    plt.savefig("exp-x2.jpg")
-    plt.show()
-    
-    
-    root = LinearBVPSolver(
-        lambda x: 0,
-        lambda x: 1,
-        lambda x: 0,
-        0, pi/2,
-        0,1,1,
-        0,1,0,
-    )
-    xs = np.linspace(0, pi/2, 100)
-    plt.plot(xs, [root(xi)-sin(xi) for xi in xs])
-    plt.title("u''+u=0,u'(0)=1,u'(pi/2)=0 (Pure Neumann)")
-    plt.savefig("sinx.jpg")
-    plt.show()
-    
-
-    root = NewtonNonlinearSolver(
-        lambda x, u, du: 2*u*du, 
-        lambda x, u, du: 2*du, 
-        lambda x, u, du: 2*u, 
-        -pi/4, pi/4,
-        1,0,-1,
-        1,0,1,
-        initial=(lambda x: x**2, lambda x: 2*x),
-        TOL=1e-13
-    )
-    xs = np.linspace(-pi/4, pi/4, 100)
-    plt.plot(xs, [root(xi)-tan(xi) for xi in xs])
-    plt.title("u''=2uu', u=tanx")
-    plt.savefig("tanx.jpg")
-    plt.show()
-    
-    
-    root = NewtonNonlinearSolver(
-        lambda x, u, du: du**2 / (2*u), 
-        lambda x, u, du: - (du / u)**2/2, 
-        lambda x, u, du: du / u, 
-        -1, 1,
-        1,0,1,
-        1,0,1,
-        initial = (lambda x: x, lambda x: 1),
-        TOL=1e-8,
-    )
-    xs = np.linspace(-1, 1, 100)
-    plt.plot(xs, [root(xi)-xi**2 for xi in xs])
-    plt.title("u''=u'^2/(2u), u=x^2")
-    plt.savefig("x^2.jpg")
-    plt.show()
-
-    root = NewtonNonlinearSolver(
-        lambda x, u, du: du * (1-du), 
-        lambda x, u, du: 0, 
-        lambda x, u, du: 1 - 2 * du, 
-        -log(2), log(2),
-        0, 1, 1/3,
-        1, 0, log(3),
-        initial = (lambda x: (x+1)/2, lambda x: 1/2),
-        TOL=1e-8,
-    )
-    xs = np.linspace(-log(2), log(2), 100)
-    plt.plot(xs, [root(xi)-log(1+exp(xi)) for xi in xs])
-    plt.title("u''=u'(1-u'), u=ln(1+exp(x))")
-    plt.savefig("logistic.jpg")
-    plt.show()
-    
